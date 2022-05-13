edf671dcc36d13c48743402816e29dc494c423c3
Giovanna Italiani <giovannaitaliani@gmail.com>
2022-05-13 09:39:14 -0300

    primeiro commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+import initScrollSuave from "./modules/scroll-suave.js";
+import animacaoInitScroll from "./modules/scroll-animacao.js";
+import initAccordion from "./modules/accordion.js";
+import initTabnav from "./modules/tabnav.js";
+import initModal from "./modules/modal.js";
+import initTooltip from "./modules/tootltip.js";
+import initDropdownMenu from "./modules/dropdown.js";
+import initMenuMobile from "./modules/menu-mobile.js";
+import initFuncionoamento from "./modules/funcionamento.js";
+import initFetchAnimais from "./modules/fetch-animais.js";
+import initFecthBitcoin from "./modules/fetch-bitcoin.js";
+
+initScrollSuave();
+animacaoInitScroll();
+initAccordion();
+initTabnav();
+initModal();
+initTooltip();
+initDropdownMenu();
+initMenuMobile();
+initFuncionoamento();
+initFetchAnimais();
+initFecthBitcoin();
